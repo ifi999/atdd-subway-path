7dc20c4c86b06add58d2e2df8e684c1259c537c5
ifi999 <kkwkjm4231@gmail.com>
2024-03-08 11:56:36 +0900

    refactor: 불필요 import 제거

--- src/test/java/nextstep/subway/unit/SectionsTest.java
+++ src/test/java/nextstep/subway/unit/SectionsTest.java
@@ -9,12 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
